37a492e1d710af8d99aea1bfc11ef1dc2fa2bb2c
Mikhail <feoktistovm88@mail.ru>
2023-01-15 23:30:30 +0300

    split the layout into components, displayed hotels from the array

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+
+import "./App.css";
+
+import {Section} from '../Section';
+
+
+function App() {
+  return (
+
+      <Section h2Text={'Homes Guest Loves'} />
+
+
+  );
+}
+
+export default App;
---
+++ src/Components/Section/Section.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from 'react';
+import { HotelCard } from '../HotelCard';
+import {hotels} from '../HotelCard/config'
+
+import './Section.css'
+
+export const Section = ({h2Text}) => (
+  <section className="homes">
+
+    <h2 className="h2-text">{h2Text}</h2>
+    <div className="homes__examples">
+
+      { hotels.map((item) => (
+        <Fragment key={item.id}>
+          <HotelCard  {...item} />
+        </Fragment>
+      ))}
+
+    </div>
+  </section>
+
+)
