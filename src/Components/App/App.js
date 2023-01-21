c104ed0701eb9512001a4c7b4f1462a86c4d5127
Mikhail <feoktistovm88@mail.ru>
2023-01-22 01:26:50 +0300

    Added Swiper Slider to project

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 
 import { Section } from "../Section";
+import { hotels } from "../HotelCard/config";
 
 function App() {
-  return <Section sectionName={"Homes guests loves"} />;
+  return <Section sectionName={"Homes guests loves"} data={hotels} />;
 }
 
 export default App;
--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { HotelCard } from "../HotelCard";
-import { hotels } from "../HotelCard/config";
 
 import "./Section.css";
 import { SliderButton } from "../SliderButton";
+import { Slider } from "../Slider";
 
-export const Section = ({ sectionName }) => (
+export const Section = ({ sectionName, data }) => (
   <section className="homes">
     <h2 className="h2-text">{sectionName}</h2>
 
-    <div className="homes__examples">
+    <div className="add">
       <SliderButton className={"s-button-next"} />
-      {hotels.map((item) => <HotelCard key={item.id} {...item} />).slice(0, 4)}
+      <SliderButton className={"s-button-prev"} />
+      <Slider className="homes__examples" data={data} />
     </div>
   </section>
 );
