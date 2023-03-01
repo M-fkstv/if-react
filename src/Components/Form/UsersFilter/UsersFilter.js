74b0e67cd4915f81b7bcd689cb49bb8782011771
Mikhail <feoktistovm88@mail.ru>
2023-03-01 23:22:16 +0300

    Select works

--- src/Components/Form/UsersFilter/UsersFilter.js
+++ src/Components/Form/UsersFilter/UsersFilter.js
@@ -13,27 +13,34 @@ export const UsersFilter = ({ active }) => {
     rooms: 0,
   });
 
- 
   return (
     <div className={active ? 'persons active' : 'persons'}>
       <div className="persons__inputs" ref={personsRef}>
         <div className="adults__input">
           <p className="inputs__title--adult">Adults</p>
-          <Counter id={'adults'} setGuests={setGuests} guests={guests} />
+          <Counter name={'adults'} setGuests={setGuests} guests={guests} />
         </div>
         <div className="children__input">
           <p className="inputs__title">Children</p>
-          <Counter id={'children'} setGuests={setGuests} guests={guests} select={select} setSelect={setSelect}/>
+          <Counter
+            name={'children'}
+            setGuests={setGuests}
+            guests={guests}
+            select={select}
+            setSelect={setSelect}
+          />
         </div>
 
         <div className="rooms__input">
           <p className="inputs__title">Rooms</p>
-          <Counter id={'rooms'} setGuests={setGuests} guests={guests} />
+          <Counter name={'rooms'} setGuests={setGuests} guests={guests} />
         </div>
-        <p className="children__input--subtitle">
-          What is the age of the child you're travelling with
-        </p>
-        <SelectWrapper select={select}  />
+        {guests.children !== 0 && (
+          <p className="children__input--subtitle">
+            What is the age of the child you're travelling with
+          </p>
+        )}
+        <SelectWrapper select={select} />
       </div>
     </div>
   );
